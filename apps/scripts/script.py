417570ea1a56bf18584b5238357bb80e13bcda2c
proxima424 <darpitrangari42@gmail.com>
2023-02-27 22:30:54 +0530

    wowww

---
+++ apps/scripts/script.py
@@ -0,0 +1,7 @@
+# The gas costs won't go into calculation
+
+# Methodology of a making a position
+#  - Deposit X ETH
+#  - Take flashloan of 2X ETH
+#  - Make position of 3X ETH on AAVE
+#  - Borrow 2X* PRICE ETH FROM 
